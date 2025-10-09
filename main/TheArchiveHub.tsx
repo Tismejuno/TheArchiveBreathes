b16b698140d315430a638ff74ecbb84e9939482c
Tismejuno <103693629+Tismejuno@users.noreply.github.com>
2025-10-09 15:12:32 +0100

    Update TheArchiveHub.tsx

--- main/TheArchiveHub.tsx
+++ main/TheArchiveHub.tsx
@@ -1,1 +1,9 @@
+import { View, Text } from 'react-native';
 
+export default function TheArchiveHub() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Welcome to The Archive Hub</Text>
+    </View>
+  );
+}
